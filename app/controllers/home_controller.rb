8a835a8ff3cfeaeabe36afbbe60e8b9b0469f38e
Mjung <mor2222@naver.com>
2018-04-30 17:26:20 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,38 @@
+class HomeController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post_title]
+    @post.content = params[:post_content]
+    @post.save
+    
+    redirect_to '/home/index'
+  end
+  
+  def destroy
+    post = Post.find(params[:post_id])
+    post.destroy
+    redirect_to '/home/index'
+  end
+  
+  def edit
+    @post = Post.find(params[:post_id])
+  end
+  
+  def update
+    post = Post.find(params[:post_id])
+    post.title = params[:post_title]
+    post.content = params[:post_content]
+    post.save
+    
+    redirect_to '/home/index'
+  end
+  
+end
+
